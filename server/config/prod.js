7a2900a3a8d540ac2f4952164faa0cef72c82d2e
kaushik2901 <kaushikjadav602@gmail.com>
2024-05-02 15:02:55 +0530

    done

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports={
-    MONGOURI:process.env.MOGOURI,
-    JWT_SECRET:process.env.JWT_SEC,
+    MONGOURI:process.env.MONGOURI,
+    JWT_SECRET:process.env.JWT_SECRET,
     EMAIL:process.env.EMAIL,
     GPASS:process.env.GPASS
 }
